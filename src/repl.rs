4f454860c39e703d5d4d38b6cd38b134c3ce884d
Willem Victor <wimpievictor@gmail.com>
2017-03-08 08:06:22 +0200

    Quitting REPL.

--- src/repl.rs
+++ src/repl.rs
@@ -5,14 +5,21 @@ use compiler;
 
 pub fn run(vm : &mut vm::VM) {
     let mut running = true;
+
+    println!("Type 'quit' to close loop...");
+
     while running {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer);
         println!("{:?}", buffer);
 
+        if buffer.contains("quit") {
+            break;
+        }
+
         let mut compiler = compiler::Compiler::new();
         compiler.compile(&buffer[..]);
 
-        running = false;
+        // running = false;
     }
 } 
